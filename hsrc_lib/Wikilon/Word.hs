5419e7b38178f865cce1eed0bee6c57d328164da
David Barbour <dmbarbour@gmail.com>
2014-10-17 12:14:47 -0500

    intern all the words

--- hsrc_lib/Wikilon/Word.hs
+++ hsrc_lib/Wikilon/Word.hs
@@ -1,24 +1,44 @@
-
+-- | Words are currently implemented as an interned UTF8 bytestring.
+--
+-- This has the following advantages:
+--
+--  * tighter memory requirements
+--  * fast comparison and hashing after intern
+--  * perfect `hash` function 
+--
+-- There are also a few disadvantages:
+--
+--  * cannot ever GC old words, even for a failed parse
+--  * non-deterministic ordering for sets or maps keyed by words
+-- 
+-- I can adapt for the latter issue, e.g. by explicitly translating
+-- word maps or sets to bytestrings when serializing transactions.
+-- 
 module Wikilon.Word
-    ( Word, textToWord, wordToText
-    , wordToUTF8
+    ( Word, textToWord, wordToText, wordToUTF8
     ) where
 
 import Data.String (IsString(..))
 import qualified Data.ByteString as B
 import qualified Codec.Binary.UTF8.Generic as UTF8
+import Data.Interned
+import Data.Interned.ByteString
+import Data.Hashable
 
 -- | A word is represented as a bytestring with UTF8 encoding.
-newtype Word = Word { unWord :: B.ByteString } deriving (Ord,Eq)
+newtype Word = Word { unWord :: InternedByteString } deriving (Ord,Eq)
 
 wordToText :: Word -> String
-wordToText = UTF8.toString . B.unpack . unWord
+wordToText = UTF8.toString . B.unpack . wordToUTF8
 
 textToWord :: String -> Word
-textToWord = Word . B.pack . UTF8.fromString 
+textToWord = Word . intern . B.pack . UTF8.fromString 
 
 wordToUTF8 :: Word -> B.ByteString
-wordToUTF8 = unWord
+wordToUTF8 = unintern . unWord
+
+-- Thoughts: I'm not going to provide the word's intern ID, since I
+-- want the ability to back out of using interning. 
 
 -- Show a Word
 instance Show Word where 
@@ -26,3 +46,8 @@ instance Show Word where
 
 instance IsString Word where
     fromString = textToWord
+
+instance Hashable Word where 
+    hashWithSalt s = hashWithSalt s . hash
+    hash = internedByteStringId . unWord
+
