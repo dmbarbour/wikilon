a6faac0d8e22176a12da6c8f25a90ec5d8587082
David Barbour <dmbarbour@gmail.com>
2015-03-25 17:41:07 -0500

    detailed design for VCache format

--- hsrc_lib/Wikilon/ABC.hs
+++ hsrc_lib/Wikilon/ABC.hs
@@ -19,12 +19,11 @@
 -- But, in the short term, we can mitigate interpreter performance by
 -- pre-processing the bytecode:
 --
---  * dictionary of built-in operators to accelerate performance
---  * support precomputed values and cheap large-value quotations 
---  * linked ABC resources (including value resources) with VCache
+--  * dictionary of accelerated operators
+--  * separate values for efficient quotation
+--  * lazy loading of large values through VCache 
 --  * fast slicing for texts, blocks, and tokens
---  * preserve base-16 compression in texts and tokens
---  * GZip compression for larger ABC texts.
+--  * GZip compression for storing ABC in VCache
 --
 -- Unlike ABCD, the internal Wikilon dictionary doesn't wait on any
 -- standards committee. Unlike {#resourceId} tokens, VCache refs are
@@ -33,7 +32,14 @@
 module Wikilon.ABC
     ( ABC(..)
     , Value(..)
-    , Rsc, Token
+    , Rsc
+    , Text
+    , Token
+
+    , ExtOp(..), extOpTable
+
+
+
     ) where
 
 import Data.Typeable
@@ -41,20 +47,30 @@ import qualified Data.Array.IArray as A
 import qualified Data.List as L
 import Data.Word
 import Data.Bits
+import qualified Data.ByteString.Builder as BB
 import qualified Data.ByteString.Lazy as LBS
+import qualified Data.ByteString.Lazy.UTF8 as LazyUTF8
+import qualified Data.ByteString.UTF8 as UTF8
 import Database.VCache
-import Wikilon.Text
+import qualified ABC.Basic as Pure
 
 
--- | Wikilon's internal ABC has two parts: the main bytecode, and
--- any large embedded values. The bytecode is not pure ABC. Rather,
--- the representations are tuned so texts and blocks can be quickly
--- sliced, and a stack of precomputed values may be loaded.
+type Token = UTF8.ByteString
+type Text = LazyUTF8.ByteString
+
+
+-- | Wikilon's internal representation of Awelon Bytecode (ABC). This
+-- encoding has several features:
+--
+--  * blocks and texts are tuned for fast slicing
+--  * extended op set to accelerate common functions
+--  * support to lazily load large values in VCache
+--  * stack of values for fast quotation and laziness
 --
 -- Equality for bytecode is inherently structural.
 data ABC = ABC
-    { abc_code :: !LBS.ByteString
-    , abc_data :: ![Value]
+    { abc_code :: !LBS.ByteString -- Wikilon's internal bytecode
+    , abc_data :: !Value          -- a stack of value resources
     } deriving (Eq, Typeable)
     -- any relationship to VCache must be modeled using a Value.
 
@@ -68,53 +84,48 @@ data ABC = ABC
 --    [a→b]   -- blocks   ~Haskell functions or arrows
 --    a{:foo} -- sealed   ~Haskell newtype
 --
--- Blocks may be affine or relevant, and other substructural types
--- are possible. Sealed values are not always discretionary like the
--- {:foo}. If cryptographically protected, they use `{$format}`. 
+-- Blocks may be affine or relevant, and other substructural types are
+-- possible. Sealed values may be cryptographic, using {$format}. 
 --
--- At the moment, Wikilon will focus on the basics forms plus:
+-- Wikilon additionally supports lazily loaded values, i.e. such that
+-- developers could model a whole multi-gigabyte filesystem as a value
+-- if they desire to do so. This is achieved using VCache.
 --
---  * dedicated support for text
---  * lazily loaded value resources
---
--- I would like to eventually have efficient vectors and matrices,
--- but those are relatively low priority.
---
--- Equality on values is structural.
+-- Tentatively, I would like in the future to optimize representations
+-- for common data structures, especially vectors and matrices.
 data Value
-    = Number {-# UNPACK #-} !Rational
+    = Number !Rational
     | Pair Value Value
     | SumL Value
     | SumR Value
     | Unit
     | Block !ABC {-# UNPACK #-} !Flags
-    | Sealed {-# UNPACK #-} !Token Value
-    | Link {-# UNPACK #-} !Rsc {-# UNPACK #-} !Flags
+    | Sealed !Token Value
+    | Linked !Rsc {-# UNPACK #-} !Flags
     deriving (Eq, Typeable)
 
-type Token = Text
-
--- | Awelon Bytecode (ABC) supports dynamic loading of bytecode resources
--- via {#resourceId} tokens in the source code. The resourceId uses a 
--- secure hash of the bytecode for provider independence, and may also
--- include a decryption key.
--- 
--- To support value resources in particular, ABC supports suffixes. For
--- example, {#resourceId'} and {#resourceId'kf} to access a plain value
--- or a linear value respectively. The type information here is coarse, 
--- just enough for whole-value data plumbing. The value may be lazily
--- loaded, rather than processed immediately. 
+-- | Awelon Bytecode (ABC) defines conventions for dynamic linking and
+-- separate compilation of resources. A {#resourceId} token identifies
+-- the resource using a secure hash and inlines the bytecode. Variants
+-- exist to support lazy loading when the resource has a value type, 
+-- i.e. type ∀e.(e→(Value*e)).
 --
--- But to avoid the heavy-weight resource IDs, Wikilon uses a VRef for
--- internal resources (serializing with a control code).
+-- But full {#resourceId} tokens are heavy-weight, and they complicate
+-- garbage collection. 
+--
+-- So Wikilon instead uses VCache, which provides implicit links, cache,
+-- and reference counting garbage collection. Sadly, this does sacrifice
+-- operation in a networked setting. But it is still very convenient and
+-- enables Wikilon to scale to very large values, e.g. whole filesystems
+-- can be modeled using a trie.
 type Rsc = VRef ABC
 
 -- | Flags for substructural types
 --   bit 0: true if relevant (no drop with %)
 --   bit 1: true if affine   (no copy with ^)
 --   bit 2..7: Reserved. Tentatively: 
---     bit 2: true if local     (no send as AVM content)
---     bit 3: true if ephemeral (no store in AVM state) 
+--     bit 2: true if local     (no send as message content)
+--     bit 3: true if ephemeral (no store in machine state)
 --
 -- Besides the basic substructural types, it might be worth using
 -- annotations to enforce a few new ones.
@@ -131,6 +142,110 @@ readVarNat = r 0 where
 
 impossible :: String -> a
 impossible eMsg = error $ "Wikilon.ABC: " ++ eMsg
+
+-- | Extended Operations are essentially a dictionary recognized by
+-- Wikilon for specialized implementations, e.g. to accelerate an
+-- interpreter or support tail-call optimizations. ExtOp is similar
+-- to ABCD, but doesn't require careful standardization.
+data ExtOp
+    -- common inline behaviors
+    = Op_Inline -- vr$c (full inline)
+    | Op_Apc    -- $c (tail call)
+
+    -- favorite fixpoint function
+    -- 
+    | Op_Fixpoint -- [^'ow^'zowvr$c]^'ow^'zowvr$c
+
+    -- mirrored v,c operations
+    | Op_Intro1L  -- vvrwlc
+    | Op_Elim1L   -- vrwlcc
+    | Op_Intro0L  -- VVRWLC
+    | Op_Elim0L   -- VRWLCC
+
+    | Op_prim_swap    -- vrwlc
+    | Op_prim_mirror  -- VRWLC
+    -- stack swaps?
+    -- hand manipulations?
+    -- more as needed!
+    deriving (Ord, Eq, Bounded, Enum, A.Ix)
+
+
+-- | Table of ExtOps and relevant semantics.
+--
+-- I'll follow ABCD's mandate to leave the ASCII range to future ABC
+-- expansions (though such expansions are very unlikely). ExtOps is
+-- effectively a prototype for ABCD.
+extOpTable :: [(ExtOp, Char, Pure.ABC)]
+extOpTable =
+    [(Op_Inline,  '¥', "vr$c")
+    ,(Op_Apc,     '¢', "$c")
+    ,(Op_Fixpoint, 'Ȳ', "[^'ow^'zowvr$c]^'ow^'zowvr$c")
+
+    ,(Op_Intro1L, 'ń', "vvrwlc")
+    ,(Op_Elim1L,  'ć', "vrwlcc")
+    ,(Op_Intro0L, 'Ń', "VVRWLC")
+    ,(Op_Elim0L,  'Ć', "VRWLCC")
+
+    ,(Op_prim_swap,   'ś', "vrwlc")
+    ,(Op_prim_mirror, 'Ś', "VRWLC")
+    ]
+
+-- QUESTION: How are resources represented in VCache?
+--
+-- Wikilon's internal ABC uses 'escapes' to access precomputed data.
+-- Thus, to properly encode our bytecode resources, we must reliably
+-- regenerate any precomputed content.
+--
+-- Following ABC's example, I'll make these implicit escapes very
+-- explicit and functionally meaningful. Each block will also pop a
+-- value because it recursively contains some precomputed data. The
+-- ability to push a value allows me to directly encode blocks and
+-- avoid recursive encodings in the 'precompute the value' sections.
+--
+--      [       pop value for encoded block
+--      DLE     pop  value resource
+--      DC1     push value resource
+--      ESC     inline a bytecode resource
+--
+-- All link resources must be pushed to the toplevel. So let's say
+-- we start with a stack of link resources, use this to compute our
+-- main values stack, then use our main values stack for our ABC.
+-- This avoids arbitrary recursion.
+--
+-- Additionally, we'll capture sizes for every block, token, and text:
+--
+--      [(Size)bytecode]
+--      {(Size)token}
+--      "(Size)textWithoutEscapes~
+--
+-- The final character isn't essential, but serves as a sanity check
+-- and a visible delimiter for debugging. In case of tokens and text, 
+-- I'll perform ABC.Base16 compression before computing the size, such
+-- that binaries at least have a compact encoding at the final storage
+-- layers.
+--
+-- The whole stream will finally be subjected to a fast compression.
+-- Candidate algorithms: LZ4, Snappy. These algorithms aren't great
+-- for compaction, but ABC should be a relatively easy target. And
+-- even a 50% compaction could make a useful performance difference
+-- on read. (Besides, decompression simply becomes part of copying
+-- the input.)
+--
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 --  * develop dictionary of built-in operators to accelerate performance
 --    * each operator has simple expansion to ABC
@@ -149,93 +264,12 @@ impossible eMsg = error $ "Wikilon.ABC: " ++ eMsg
 --    * {(length)(content)}
 --    * length and links have VarNat representation
 --    * escapes are removed from the text content
---  * precompress ABC's Base16 for texts and tokens (tune text type)
---  * GZip compression of larger ABC at VCache layer (option?)
+--  * Compression of larger ABC at VCache layer (option?)
 --    * combine texts for precomputed values and ABC for best compression
+--    * Base16 compression for tokens and texts
 --
 
 {-
-
--- | Awelon Bytecode is simply a sequence of operations.
-newtype ABC = ABC { abcOps :: [Op] }
-    deriving (Eq, Typeable)
--- Thoughts: a list isn't ideal for modeling composition as concatenation.
--- However, we can alternatively represent composition using a block with
--- the inline function (vr$c).
---
--- Also, I'm not sure it wouldn't be better to simply process bytestrings
--- directly... excepting that I need to model values, and it would be useful
--- to know exactly how large to slice
-
--- | Wikilon's internal variant of ABC for efficient computation.
-data Op 
-    = ABC_Prim !PrimOp   -- ABC primitives
-    | ABC_Ext  !ExtOp    -- extended set of single-character operators
-    | ABC_Val  !Value    -- texts, blocks, values (∀e.e→(Val * e)).
-    | ABC_Lnk  {-# UNPACK #-} !Rsc {-# UNPACK #-} !TyF -- internal {#resourceId} 
-    | ABC_Tok  {-# UNPACK #-} !Token -- {token} text
-    deriving (Eq, Typeable)
--- Thoughts:
---   for performance, it might be better to use `ABC_Prims [PrimOp]`, or
---   even a vector of primitives, or perhaps even a bytestring (since each
---   prim may be encoded using one byte). This could improve locality when
---   processing code
---
---   also, is this the best way to model internal resources? I'm not sure.
-
-
-
--- | Extended Operations are essentially a dictionary recognized by
--- Wikilon for specialized implementations, e.g. to accelerate an
--- interpreter or support tail-call optimizations. ExtOp is similar
--- to ABCD, but doesn't require careful standardization.
---
--- A dictionary of extended ops shall be available for HTTP access
--- via something like a \/builtins request. 
-data ExtOp
-    -- common inline behaviors
-    = Op_Inline -- vr$c (full inline)
-    | Op_Apc    -- $c (a useful tail-call)
-
-    -- favorite fixpoint function
-    | Op_FixHalf --  ^'ow^'zowvr$c 
-    | Op_FixFull -- [^'ow^'zowvr$c]^'ow^'zowvr$c
-
-    -- mirrored v,c operations
-    | Op_Intro1L  -- vvrwlc
-    | Op_Elim1L   -- vrwlcc
-    | Op_Intro0L  -- VVRWLC
-    | Op_Elim0L   -- VRWLCC
-
-    | Op_prim_swap    -- vrwlc
-    | Op_prim_mirror  -- VRWLC
-    -- stack swaps?
-    -- hand manipulations?
-    -- more as needed!
-    deriving (Ord, Eq, Bounded, Enum, A.Ix)
-
--- | Table of ExtOps and relevant semantics.
---
--- I've decided to follow ABCD's mandate to leave the ASCII range to
--- future ABC expansions. ExtOps is effectively a prototype for ABCD.
-extOpTable :: [(ExtOp, Char, Pure.ABC)]
-extOpTable =
-    [(Op_Inline,  '¥', "vr$c")
-    ,(Op_Apc,     '¢', "$c")
-    ,(Op_FixHalf, 'ȳ',  "^'ow^'zowvr$c")
-    ,(Op_FixFull, 'Ȳ', "[^'ow^'zowvr$c]^'ow^'zowvr$c")
-
-    ,(Op_Intro1L, 'ǔ', "vvrwlc")
-    ,(Op_Elim1L,  'ç', "vrwlcc")
-    ,(Op_Intro0L, 'Ǔ', "VVRWLC")
-    ,(Op_Elim0L,  'Ç', "VRWLCC")
-
-    ,(Op_prim_swap,   'ƨ', "vrwlc")
-    ,(Op_prim_mirror, 'Ƨ', "VRWLC")
-    ]
-
-impossible :: String -> a
-impossible eMsg = error ("Wikilon.ABC: " ++ eMsg)
 
 extOpCharArray :: A.Array ExtOp Char 
 extOpCharArray = A.accumArray ins eUndef (minBound, maxBound) lst where
@@ -268,97 +302,8 @@ extCharToOp c | inBounds = extCharOpArray A.! c
     where inBounds = (lb <= c) && (c <= ub)
           (lb,ub) = A.bounds extCharOpArray
 
-
-
--- | Values in Awelon project (and ABC) have a few basic forms:
---
---    (a * b) -- pairs    ~Haskell (a,b)
---    (a + b) -- sums     ~Haskell (Either a b)
---    1       -- unit     ~Haskell ()
---    0       -- void     ~Haskell EmptyDataDecls
---    N       -- numbers  ~Haskell Rational
---    [a→b]   -- blocks   ~Haskell functions or arrows
---    a:foo   -- sealed   ~Haskell newtype (`:foo` label)
---
--- Further, blocks may be affine or relevant, ABC defines a format
--- for cryptographically sealed values, and high performance compact
--- data structures (e.g. vectors and matrices) could be used under
--- the hood with some clever annotations and extended operations. 
---
--- At the moment, Wikilon will focus on the basics plus:
---
---  * dedicated support for text
---  * lazily loaded value resources
---
--- I would like to eventually have efficient vectors, matrices, and
--- cryptographic content. But a lot of these ideas can easily wait.
---
-data Value
-    = Number {-# UNPACK #-} !Rational
-    | Pair !Value Value
-    | SumL Value
-    | SumR Value
-    | Unit
-    | Block !ABC {-# UNPACK #-} !TyF
-    | Sealed {-# UNPACK #-} !Token !Value
-    | Load {-# UNPACK #-} !Rsc {-# UNPACK #-} !TyF
-    deriving (Eq, Typeable)
-
-
-
 -}
 
-
-
-
 {-
-
-
-import Control.Applicative ((<$>), pure)
-import Control.Monad (join)
-import Control.Exception (assert)
-import qualified Data.Binary as B
-import qualified Data.Binary.Get as B
-import qualified Data.Binary.Put as B
-import qualified Codec.Binary.UTF8.Generic as UTF8
-import qualified Data.List as L
-import qualified Data.Array.Unboxed as A
-import Data.Ratio
-import Data.Word (Word16)
-import Data.String
-import Wikilon.Char
-import qualified Wikilon.ParseUtils as P
-
-
--- NOTE: Binaries can be embedded in ABC text or tokens by use of a specialized 
--- base16 alphabet: bdfghjkmnpqstxyz. This is a-z minus the vowels and `vrwlc` 
--- data plumbing. A special compression pass then encodes binaries with 0.8%
--- overhead (for large binaries) compared to a raw encoding. Some binaries can
--- be further compressed by the normal LZSS compression pass.
-
-skip :: a -> b -> b
-skip = flip const
-
--- help type inference a little
-primOp :: PrimOp -> Op
-primOp = ABC_Prim
-
-
-putABC :: (Quotable ext) => [ABC_Op ext] -> B.PutM ()
-putABC = mapM_ putOp
-
-putOp :: (Quotable ext) => ABC_Op ext -> B.PutM ()
-putOp (ABC_Prim op) = B.put (abcOpToChar op)
-putOp (ABC_Block abc) = B.put '[' >> putABC abc >> B.put ']'
-putOp (ABC_Text txt) = B.put '"' >> putMLT txt >> B.put '\n' >> B.put '~'
-putOp (ABC_Tok tok) = assert (validTok tok) $ B.put '{' >> mapM_ B.put tok >> B.put '}'
-putOp (ABC_Ext ops) = putABC (quote ops)
-
-putMLT :: String -> B.PutM ()
-putMLT ('\n':cs) = B.put '\n' >> B.put ' ' >> putMLT cs
-putMLT (c:cs) = B.put c >> putMLT cs
-putMLT [] = return ()
-
-
-
+Todo: Quotation of Wikilon's ABC back into pure ABC
 -}
