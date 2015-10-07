0d5bc407df0a5ec00fc83b510039c22e75303251
David Barbour <dmbarbour@gmail.com>
2015-10-07 15:23:15 -0500

    (broken commit) adapting old Claw code to new model; adding some punctuation features for list encodings

--- wikilon-model/hsrc/Wikilon/ABC.hs
+++ wikilon-model/hsrc/Wikilon/ABC.hs
@@ -1,7 +1,21 @@
 
--- | Optimized ABC model used within Wikilon.
+-- | While Awelon Bytecode may be naively interpreted, doing so is
+-- not efficient. Wikilon will pre-process bytecode to support many
+-- performance tweaks:
+--
+-- * bytecode held in relatively compact bytestrings
+-- * fast slicing for texts, blocks, tokens in bytecode
+-- * quoted values may be computed once ahead of time
+-- * faster copy via copyable annotations for values
+-- * larger-than-memory values supportable via VCache
+-- * shared structure for large values via VCache
+-- * ABCD-like extended dictionary of accelerated ops
+-- 
+-- Consequently, Wikilon ABC is a bit specialized regarding the
+-- value types involved. This module focuses on 'true' value
+-- encodings. Though, I'm also interested in type-oriented 
+-- value encodings.
 module Wikilon.ABC 
     (
     ) where
-
 
