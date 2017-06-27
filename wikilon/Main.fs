30e7a6bbfadbbd104c3ba4f503f4d256af96091f
David Barbour <dmbarbour@gmail.com>
2017-06-27 18:24:18 -0500

    first working F# solution for MonoDevelop (empty GTK project; console etc. did not work)

---
+++ wikilon/Main.fs
@@ -0,0 +1,14 @@
+﻿namespace wikilon
+    module Main =
+        
+        open System
+        open Gtk
+    
+        [<EntryPoint>]
+        let Main(args) = 
+            Application.Init()
+            let win = new MainWindow.MyWindow()
+            win.Show()
+            Application.Run()
+            0
+
