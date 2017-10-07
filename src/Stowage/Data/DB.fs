fe2f3bf119898a005241121f3d1548c75162be2d
David Barbour <dmbarbour@gmail.com>
2017-10-07 01:10:22 -0500

    fixed up compilation

--- src/Stowage/Data/DB.fs
+++ src/Stowage/Data/DB.fs
@@ -65,26 +65,16 @@ type DB =
 
     /// Perform Hierarchical Transaction.
     ///
-    /// This runs a transaction using a snapshot view of the DB. Upon
-    /// normal return, the transactional update is committed. Commit
-    /// may fail if a concurrent transaction has written a TVar that
-    /// the transaction read at a different value. The extra boolean
-    /// result reports whether a transaction commited successfully.
-    ///
-    /// Transactions at the DB layer should be optimistic, without 
-    /// risk of deadlock. Optimistic transactions aren't appropriate
-    /// for high-contention variables, but synchronization is left 
-    /// to another layer of indirection.
+    /// This creates a transactional model of the DB interface, such
+    /// that read dependencies are tracked and writes are aggregated.
+    /// Ideally has snapshot isolation, so read-only transactions do
+    /// not fail. Commit is implicit on returning, and may fail. The
+    /// extra boolean result reports commit success.
+    /// 
+    /// For high-contention variables, clients should use external
+    /// synchronization to improve chances of transaction success.
     abstract member Transact : (DB -> 'X) -> struct('X * bool)
 
-    /// Check Transaction for Obvious Causes of Failure
-    ///
-    /// Return `false` only if commit would fail. False optimism
-    /// is permitted - the trivial implementation always returns
-    /// `true`. Purpose is to help clients short-circuit a long
-    /// running transaction where interference is possible but is
-    /// rare enough that optimism is warranted.
-    abstract member Check : unit -> bool
 
 module DB =
 
@@ -113,12 +103,15 @@ module DB =
         // GOAL: Create a DB from a Storage!
 
         // create unique IDs for DBVars to simplify indexing.
+        //
+        // I assume we won't process the whole 64-bit space, at least
+        // without a few hundred years of runtime without restarting.
         type UID = uint64
         type IDGen =
             val mutable private prev : UID
             new() = { prev = 0UL }
             member g.Next() = lock g (fun () -> 
-                g.prev <- (1UL + g.pref) 
+                g.prev <- (1UL + g.prev) 
                 g.prev) 
         let idGen = new IDGen()
 
@@ -134,12 +127,12 @@ module DB =
             | Durable of Key * (Data -> Val)
             | Ephemeral
 
-        let rootDur (k:Key) (cV:Codec<'V>) = 
+        let rootDur (k:Key) (cV:Codec<'V>) : Durability = 
             let s d = 
                 match unbox<'V option>(d) with
                 | None -> None
                 | Some v -> Some (Codec.writeBytes cV v)
-            Durable k s
+            Durable(k,s)
 
         type DBVar = 
             val UID : UID
@@ -171,8 +164,9 @@ module DB =
             inherit DBVar(box<'V>(v),dur)
             interface TVar<'V>
 
-        allocEph (v:'V) = new DBVar<'V>(v,Ephemeral)
-        allocRoot (db:Storage) (k:Key) (cV:Codec<'V>) : DBVar<'V option> =
+        let allocEph (v:'V) = new DBVar<'V>(v, Ephemeral)
+
+        let allocRoot (db:Storage) (k:Key) (cV:Codec<'V>) : DBVar<'V option> =
             let parse b = Codec.readBytes cV (db :> Stowage) b
             let vOpt = Option.map parse (db.Read k)
             new DBVar<'V option>(vOpt, rootDur k cV)
@@ -210,6 +204,7 @@ module DB =
 
         type Reads = Map<DBVar,Data>
         type Writes = Reads
+        type Env = Reads
 
         // compute serializable writes (excludes ephemerals)
         let serializeWrites (ws:Writes) : KVMap =
@@ -219,7 +214,10 @@ module DB =
                 | Ephemeral -> wb
             Map.fold add (BTree.empty) ws
 
-
+        // quickly merge two maps, favoring values from `a` on conflict.
+        let inline leftBiasedUnion (a:Map<'K,'V>) (b:Map<'K,'V>) : Map<'K,'V> =
+            if Map.isEmpty b then a else
+            Map.foldBack (Map.add) a b
 
         // Snapshots for transactional updates.
         //
@@ -234,8 +232,8 @@ module DB =
         // hierarchical transactions should all use the same image.
         [<AllowNullLiteral>]
         type Snapshot =
-            val mutable next   : Snapshot      // linked list iteration
-            val mutable refct  : nativeint     // sharing of data 
+            val mutable next   : Snapshot   // linked list iteration
+            val mutable refct  : nativeint  // sharing of data 
             val mutable memory : Map<UID,Data> // remembered history
 
             new() = 
@@ -244,24 +242,26 @@ module DB =
                   memory = Map.empty
                 }
 
-            member ss.Read (dbv:DBVar) : Data = lock ss (fun () ->
+            member ss.ReadData (dbv:DBVar) : Data = lock ss (fun () ->
                 match Map.tryFind (dbv.UID) (ss.memory) with
                 | Some data -> data
                 | None -> dbv.Data)
 
+            // for variables we're about to write and for which we lack
+            // any memory, add pre-write value (dbv.Data) to snapshot.
             member ss.Precommit (ws:Writes) : unit = lock ss (fun () ->
                 if (0n = ss.refct) then () else
-                let add mem (dbv:DBVar) _ =
+                let add (dbv:DBVar) _  mem =
                     let k = dbv.UID
-                    if Map.containsKey k mem then mem else
-                    Map.add k (dbv.Data) mem
-                ss.mem <- Map.fold add (ss.mem) ws)
-        
+                    if Map.containsKey k mem 
+                        then mem 
+                        else Map.add k (dbv.Data) mem
+                ss.memory <- Map.foldBack add ws (ss.memory))
 
         // Primary DB implementation.
         type RootDB =
             val store : Storage
-            val mutex : System.Object
+            val private mutex : System.Object
             val mutable ss : Snapshot
             val mutable roots : RootsCache
             val mutable buffer : Writes
@@ -291,7 +291,7 @@ module DB =
                 let b = db.Commit (Map.empty) ws
                 assert(b)
 
-            member db.Commit (rs:Reads) (ws:Writes) : bool =
+            member private db.Commit (rs:Reads) (ws:Writes) : bool =
                 // snapshot consistency implies read-only transactions succeed,
                 // logically occurring before any change in the data.
                 if Map.isEmpty ws then true else
@@ -302,26 +302,27 @@ module DB =
                     if not okReads then false else
                     db.Precommit ws
                     let commit (dbv:DBVar) v = 
-                        dbv.data <- v
+                        dbv.Data <- v
                         if isDurable dbv // buffer the durable writes
-                            then db.buffer <- Map.add dbv v (db.buffer)
+                           then db.buffer <- Map.add dbv v (db.buffer)
                     Map.iter commit ws
                     true)
             
+            // precommit assumes we're holding lock
             member private db.Precommit (ws:Writes) : unit =
-                let rec loop ss =
+                let rec loop (ss:Snapshot) =
                     if (null = ss.next) then () else
                     if (0n = ss.next.refct) 
-                       then ss.next <- ss.next.next // drop unused
+                       then ss.next <- ss.next.next // drop unused, old
                             loop ss
-                       else ss.next.Precommit ws // update in use
+                       else ss.next.Precommit ws // update snapshot
                             loop (ss.next)
                 // introduce fresh snapshot per commit. Transactions 
                 // started before next commit use the fresh snapshot.
                 let ss = new Snapshot()
                 ss.next <- db.ss
+                loop ss
                 db.ss <- ss
-                loop (db.ss)
 
             // Register a durable variable.
             //
@@ -333,16 +334,16 @@ module DB =
             // shouldn't be a huge issue in practice.
             member db.Register (kRaw:ByteString) (cV:Codec<'V>) : DBVar<'V option> =
                 let k = BS.trimBytes (db.store.Mangle kRaw)
-                match tryFindRoot<'V> k (db.cache) with
+                match tryFindRoot<'V> k (db.roots) with
                 | Some dbv -> dbv
                 | None ->
                     let dbv = allocRoot (db.store) k cV
                     lock (db.mutex) (fun () ->
-                        match tryFindRoot<'V> k (db.cache) with
+                        match tryFindRoot<'V> k (db.roots) with
                         | Some dbvRaceWinner -> dbvRaceWinner
                         | None -> 
                             let wref = new System.WeakReference(dbv)
-                            db.cache <- BTree.add k wref (db.cache)
+                            db.roots <- BTree.add k wref (db.roots)
                             dbv
                     )
 
@@ -354,6 +355,7 @@ module DB =
                     let tm = System.GC.CollectionCount(1)
                     if (tm = db.tm_clean) then () else
                     db.tm_clean <- tm
+                    db.Precommit (Map.empty)
                     db.roots <- clearCache (db.roots)
                 )
 
@@ -361,7 +363,8 @@ module DB =
             //
             // goals: concurrent flush from multiple threads should
             // leverage parallelism for serialization and may overlap
-            // in time (instead of acting as sequential flushes).
+            // in time (instead of acting as sequential flushes). No
+            // serializing while holding mutex.
             // 
             // To achieve this, I use `flushing` to ensure sequential
             // order for write-batch submissions, with each thread first
@@ -369,27 +372,26 @@ module DB =
             // And the backend is flushed after we've submitted in order.
             member db.Flush() : unit =
                 let flush = lock (db.mutex) (fun () ->
-                    let prior_flush = db.flushing
                     let wb = db.buffer
                     db.buffer <- Map.empty
+                    let prior_flush = db.flushing
                     db.flushing <- lazy (
-                        let ws = serializeWrites wb // potentially concurrent with prior_flush
-                        prior_flush.Force()         // wait for prior flush to submit write batch
-                        db.store.WriteBatch ws      // submit the serial batch we created
-                        System.GC.KeepAlive(wb))    // hold onto DBVars until updated values are visible
+                        let ws = serializeWrites wb // concurrent with prior
+                        prior_flush.Force()         // wait for prior Write
+                        db.store.WriteBatch ws      // write updates
+                        System.GC.KeepAlive(wb))    // guard DBVars, VRefs
                     db.flushing)
                 flush.Force()    // serialize and submit our write batch
                 db.store.Flush() // flush write batch to durable storage
                 db.Cleanup()     // potential cleanup of roots cache
             
-            interface Stowage.DB with
+            interface DB with
                 member db.Register k cV = upcast (db.Register k cV)
-                member __.Allocate v = allocEph v
+                member __.Allocate v = upcast (allocEph v)
                 member db.Read tv = db.Read (castTVar tv)
                 member db.Write tv v = db.Write (castTVar tv) v 
-                member db.Flush () = db.Flush()
+                member db.Flush () = db.Flush ()
                 member db.Transact fn = db.Transact fn
-                member db.Check () = true
 
             // obtain snapshot from most recent commit
             member db.AcquireSnapshot () : Snapshot =
@@ -409,8 +411,8 @@ module DB =
             member db.Transact (fn : DB -> 'X) : struct('X * bool) =
                 let (result,rs,ws,flush) = 
                     let ss = db.AcquireSnapshot()
-                    try let tx = new TX(db,ss)
-                        let r = fn (tx :> DB)
+                    try let tx = new TX(db,ss,Map.empty)
+                        let result = fn (tx :> DB)
                         (result, tx.rs, tx.ws, tx.flush)
                     finally db.ReleaseSnapshot ss
                 let ok = db.Commit rs ws
@@ -420,14 +422,16 @@ module DB =
         and TX = 
             val db : RootDB     // for stowage, register, allocate
             val ss : Snapshot   // for new reads
-            val mutex : System.Object 
+            val env : Reads     // modifier for parent TX writes 
+            val private mutex : System.Object 
             val mutable rs : Reads
             val mutable ws : Writes
             val mutable flush : bool
 
-            new(db,ss) =
+            new(db,ss,env) =
                 { db = db
                   ss = ss
+                  env = env
                   rs = Map.empty
                   ws = Map.empty
                   flush = false
@@ -441,32 +445,91 @@ module DB =
                 member tx.Incref h = tx.Stowage.Incref h
                 member tx.Decref h = tx.Stowage.Decref h
 
-            // reads on a 
-            member tx.Read (dbv:DBVar<'V>) : 'V = lock (tx.mutex) (fun () ->
-                let k = (dbv :> DBVar)
-                match Map.tryFind k (tx.ws) with
-                | Some data -> unbox<'V>(data)
-                | 
 
-                let k = (dbv :> DBVar)
-                match Map.tryFind k (tx.ws) with
-                | Some 
+            // reads in a transaction will look in several locations:
+            //
+            //  - the write set (since we might have updated it)
+            //  - the read set (avoid extra lock on snapshot)
+            //  - environment (our parent might have written it)
+            //  - the snapshot (for snapshot-consistent data)
+            //
+            // Our environment represents the writes present from the
+            // parent TX. Assuming the parent TX might be concurrently
+            // updated, we must record dependencies at this environment.
+            member tx.ReadData (dbv:DBVar) : Data =
+                lock (tx.mutex) (fun () ->
+                    match tx.ReadLocalData dbv with
+                    | Some data -> data
+                    | None -> tx.ReadEnvData dbv)
 
-tx.ss.Read dbv
+            member private tx.ReadLocalData (dbv:DBVar) : Data option =
+                let inWS = Map.tryFind dbv (tx.ws)
+                if Option.isSome inWS then inWS else
+                Map.tryFind dbv (tx.rs)
+
+            member private tx.ReadEnvData (dbv:DBVar) : Data =
+                let data = 
+                    match Map.tryFind dbv (tx.env) with
+                    | Some data -> data
+                    | None -> tx.ss.ReadData dbv
+                tx.rs <- Map.add dbv data (tx.rs)
+                data
+
+            member inline tx.Read (dbv:DBVar<'V>) : 'V = 
+                unbox<'V>(tx.ReadData (dbv :> DBVar))
                 
+            member tx.Write (dbv:DBVar<'V>) (v:'V) : unit =
+                lock (tx.mutex) (fun () ->
+                    tx.ws <- Map.add (dbv :> DBVar) (box<'V>(v)) (tx.ws))
 
-            interface Stowage.DB with
-                member db.Register k cV = upcast (db.Register k cV)
-                member __.Allocate v = allocEph v
-                member db.Read tv = db.Read (castTVar tv)
-                member db.Write tv v = db.Write (castTVar tv) v 
-                member db.Flush () = db.Flush()
-                member db.Transact fn = db.Transact fn
-                member db.Check () = true
+            member inline tx.Flush() = tx.flush <- true
 
-    
-            interface 
-    
+            interface DB with
+                member tx.Register k cV = upcast (tx.db.Register k cV)
+                member __.Allocate v = upcast (allocEph v)
+                member tx.Read tv = tx.Read (castTVar tv)
+                member tx.Write tv v = tx.Write (castTVar tv) v 
+                member tx.Flush () = tx.Flush ()
+                member tx.Transact fn = tx.Transact fn
 
+            // hierarchical transactions
+            member tx.Transact (fn : DB -> 'X) : struct('X * bool) =
+                let env = leftBiasedUnion (tx.ws) (tx.env)
+                let ctx = new TX(tx.db, tx.ss, env)
+                let result = fn (ctx :> DB)
+                let ok = tx.Commit (ctx.rs) (ctx.ws)
+                if (ok && ctx.flush) then tx.Flush()
+                struct(result,ok)
+
+            // hierarchical transaction commit
+            //
+            // A transaction may fail if the parent TX was concurrently
+            // updated in a manner that invalidates a read by the child.
+            // If the transaction succeeds, we can filter our read-set
+            // based on the parent's writes (i.e. we logically read from
+            // the point of commit to the TX except for a read-only TX).
+            member private tx.Commit (rs:Reads) (ws:Writes) : bool =
+                // snapshot consistency implies read-only transactions succeed
+                if Map.isEmpty ws then true else
+                lock (tx.mutex) (fun () ->
+                    let check (dbv:DBVar) v =
+                        match Map.tryFind dbv (tx.ws) with
+                        | Some data -> System.Object.ReferenceEquals(data,v)
+                        | None -> true // no conflict here
+                    let okReads = Map.forall check rs
+                    if not okReads then false else
+                    let addReadDep dbv v m =
+                        if Map.containsKey dbv (tx.ws) then m else
+                        Map.add dbv v m
+                    tx.rs <- Map.foldBack addReadDep rs (tx.rs)
+                    tx.ws <- leftBiasedUnion ws (tx.ws)
+                    true)
+
+    /// Create a DB from a Storage.
+    ///
+    /// Assumes exclusive write access to the given Storage. This is
+    /// a relatively convenient way to create a full Stowage DB.
+    let fromStorage (s:Storage) : DB =
+        upcast (new StorageDB.RootDB(s))
 
 
