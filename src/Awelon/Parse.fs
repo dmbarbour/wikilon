d2579438cc7209ba812510ee17a977e22371479b
David Barbour <dmbarbour@gmail.com>
2018-07-16 18:36:27 -0500

    adding cursor for rewriting programs

--- src/Awelon/Parse.fs
+++ src/Awelon/Parse.fs
@@ -6,16 +6,14 @@ open Stowage
 
 // Awelon is encoded in ASCII (minus C0 and DEL), and is syntactically
 // simple, similar to Forth. A program is a sequence of named operations
-// with first-class blocks (which contain programs) and namespaces. The
-// operations are mostly user-defined words, but we'll also support some
-// syntactic sugars (for embedded texts, natural numbers), annotations,
-// and external binary or code resources.
+// with first-class blocks (which contain programs) and namespaces. We 
+// also enable a hierarchical dictionary namespace.
 //
 //   Program = Action*
 //   Action = Block | NS '/' Action | Atom
 //   Block = '[' Program ']'
 //   NS = Word
-//   Atom = Word | Annotation | Nat | Text | Resource | Label 
+//   Atom = Word | Annotation | Nat | Text | Resource
 //
 //   Word = [a-z][a-z0-9-]* 
 //   Annotation = '(' Word ')'
@@ -25,9 +23,6 @@ open Stowage
 //   BinRef = '%' RscHash
 //   CodeRef = '$' RscHash
 //   RscHash = (see Stowage.RscHash)
-//   Label = LabelPut | LabelGet
-//   LabelPut = ':' Word
-//   LabelGet = '.' Word
 // 
 // This module implements a hand-written parser for Awelon code. 
 module Parser =
@@ -55,29 +50,42 @@ module Parser =
         | Anno=1    // (anno)
         | Nat=2     // 0 42 128
         | Text=3    // "text"
-        | LblPut=4  // :label
-        | LblGet=5  // .label
-        | BinRef=6  // %secureHash
-        | CodeRef=7 // $secureHash
+        | BinRef=4  // %secureHash
+        | CodeRef=5 // $secureHash
+        //| LblPut=6  // :label
+        //| LblGet=7  // .label
     type Token = (struct(TT * ByteString))
     let inline tok tt s = struct(tt,s)
     let inline tokWord w = tok TT.Word w
     let inline tokAnno w = tok TT.Anno w
     let inline tokNat  n = tok TT.Nat n
     let inline tokText t = tok TT.Text t
-    let inline tokLblPut w = tok TT.LblPut w
-    let inline tokLblGet w = tok TT.LblGet w
     let inline tokBinRef h = tok TT.BinRef h
     let inline tokCodeRef h = tok TT.CodeRef h
 
     /// At this Parser layer, a Program is simply a legal parse. There
-    /// is no association with the dictionary context.
+    /// is no association with the dictionary or Stowage context.
     type Program = Action list  // Action*
+        // TODO: consider the ImmutableArray<Action> type, instead.
+        // That should be more efficient in many use cases. But the
+        // list is simpler to work with from F#.
     and Action =
-        | Block of Program      // [Program]
-        | NS of Word * Action   // ns/Action
         | Atom of Token         // simple action
-        // TODO: consider a vector (immutable array) type
+        | Block of Program      // [Program]
+        | NS of NSAction        // ns/Action
+    and NSAction = (struct(Word * Action))
+
+    /// Test whether an Action is a Block, possibly with namespace.
+    let rec isBlock op =
+        match op with
+        | Block _ -> true
+        | NS (ns,op') -> isBlock op'
+        | Atom _ -> false
+
+    let rec private wrapNS ns op =
+        match ns with
+        | (w::ns') -> wrapNS ns' (NS(struct(w,op)))
+        | _ -> op
 
     /// Awelon uses characters in ASCII minus C0 and DEL. Most
     /// of these are permitted only within embedded texts. For
@@ -123,7 +131,7 @@ module Parser =
 
     let isValidToken (struct(tt,s) : Token) : bool =
         match tt with
-        | TT.Word | TT.Anno | TT.LblPut | TT.LblGet -> isValidWord s
+        | TT.Word | TT.Anno -> isValidWord s
         | TT.Text -> isValidText s
         | TT.Nat -> isValidNatTok s
         | TT.BinRef | TT.CodeRef -> RscHash.isValidHash s
@@ -143,29 +151,32 @@ module Parser =
                 | [] -> true
         loop [] p0
 
+    /// Parser state, mostly tracks block and namespace context. 
+    [<Struct>]
+    type ParseState =
+        { cx    : ParseState option // parent block parse context
+          ns    : Word list     // pending operation's namespace
+          p     : Action list   // reverse-ordered list of actions in block
+        }
+    let inline makeParseState cx ns p = 
+        { cx = cx; ns = ns; p = p }
+    let inline parsedOp st op =
+        let ns_op = wrapNS (st.ns) op // add pending ns to operation
+        makeParseState (st.cx) [] (ns_op::(st.p))
+    let inline parsedTok st tok = parsedOp st (Atom tok)
+    let inline parsedNS st w = makeParseState (st.cx) (w::st.ns) (st.p)
+
     /// On Parse Error, we'll return the parser's full state.
     /// The lists in this case represent stacks, e.g. `p` is
     /// reverse order relative to the program it represents.
-    type ParseState =
-        { cx    : (struct(Word list * Action list)) list // block context
-          ns    : Word list     // pending operation's namespace
-          p     : Action list   // current open block
-          s     : ByteString    // remaining bytes
-        }
-
     type ParseResult =
         | ParseOK of Program
-        | ParseFail of ParseState
+        | ParseFail of (ParseState * ByteString)
     
-    let private finiParse cx ns p s =
-        let ok = BS.isEmpty s && List.isEmpty cx && List.isEmpty ns
-        if ok then ParseOK (List.rev p) else
-        ParseFail { cx = cx; ns = ns; p = p; s = s }
-
-    let rec private wrapNS (ns : Word list) (a:Action) : Action =
-        match ns with
-        | [] -> a
-        | (w::ns') -> wrapNS ns' (NS(w,a))
+    let private finiParse st s =
+        let ok = BS.isEmpty s && Option.isNone (st.cx) && List.isEmpty (st.ns)
+        if ok then ParseOK (List.rev (st.p)) else
+        ParseFail (st,s)
 
     let inline private matchChar c s = 
         startsWith ((=) (byte c)) s
@@ -196,14 +207,6 @@ module Parser =
             | Some (struct(w,s')) when matchChar ')' s' ->
                 Some (struct(tokAnno w, BS.unsafeTail s'))
             | _ -> None
-        else if(byte ':' = c0) then // record label 
-            match tryParseWord (BS.unsafeTail s) with
-            | Some (struct(w,s')) -> Some (struct(tokLblPut w, s'))
-            | _ -> None
-        else if(byte '.' = c0) then // record label access
-            match tryParseWord (BS.unsafeTail s) with
-            | Some (struct(w,s')) -> Some (struct(tokLblGet w, s'))
-            | _ -> None
         else if(byte '%' = c0) then // binary resource
             match tryParseHash (BS.unsafeTail s) with
             | Some (struct(h,s')) -> Some (struct(tokBinRef h, s'))
@@ -214,38 +217,33 @@ module Parser =
             | _ -> None
         else None
 
-    // parse given full parse state 
-    let rec parse' cx ns p s =
-        if BS.isEmpty s then finiParse cx ns p s else
+    /// parse given a full parse state and remaining data. 
+    let rec parse' (st:ParseState) (s:ByteString) : ParseResult =
+        if BS.isEmpty s then finiParse st s else
         let c0 = BS.unsafeHead s
-        if ((byte ' ' = c0) && (List.isEmpty ns)) then
+        if ((byte ' ' = c0) && (List.isEmpty (st.ns))) then
             // whitespace is permitted between actions
-            // (but not between a namespace and action)
-            parse' cx [] p (BS.unsafeTail s)
+            //   but not between namespace and action
+            parse' st (BS.unsafeTail s)
         else if(byte '[' = c0) then
-            // save namespace for ']', save context to addend block
-            parse' (struct(ns,p)::cx) [] [] (BS.unsafeTail s)
-        else if((byte ']' = c0) && (List.isEmpty ns)) then
+            let st' = makeParseState (Some st) [] []
+            parse' st' (BS.unsafeTail s)
+        else if((byte ']' = c0) && (List.isEmpty (st.ns))) then
             // require empty ns to forbid `foo/bar/]` nonsense.
-            match cx with
-            | (struct(ns',p')::cx') ->
-                let a = wrapNS ns' (Block(List.rev p))
-                parse' cx' [] (a :: p') (BS.unsafeTail s)
-            | [] -> finiParse cx ns p s
+            match st.cx with
+            | Some bcx ->
+                let op = Block (List.rev (st.p))
+                parse' (parsedOp op bcx) (BS.unsafeTail s)
+            | None -> finiParse st s
         else if isWordStart c0 then
             let struct(w,s') = BS.span isWordChar s
-            if matchChar '/' s' then
-                // word is namespace prefix
-                parse' cx (w :: ns) p (BS.unsafeTail s')
-            else
-                // word is normal action
-                let a = wrapNS ns (Atom (tokWord w))
-                parse' cx [] (a :: p) s'
+            if matchChar '/' s' 
+                then parse' (parsedNS st w) (BS.unsafeTail s')
+                else parse' (parsedTok st (tokWord w)) s'
         else 
             match tryParseToken s with
-            | Some (struct(a,s')) ->
-                parse' cx [] ((wrapNS ns (Atom a)) :: p) s'
-            | None -> finiParse cx ns p s
+            | Some (struct(tok,s')) -> parse' (parsedTok st tok) s'
+            | None -> finiParse st s
 
     /// Parse from a ByteString.
     ///
@@ -254,12 +252,9 @@ module Parser =
     /// code, do so at the dictionary layer to simplify caching, forking,
     /// versioning, review, undo, and editing of the stream. Individual
     /// definitions should still be small.
-    let inline parse (s:ByteString) : ParseResult = parse' [] [] [] s
-
-    /// Parse from modified parse state.
-    let inline parseST (st:ParseState) : ParseResult =
-        parse' (st.cx) (st.ns) (st.p) (st.s)
-
+    let inline parse (s:ByteString) : ParseResult =
+        let st0 = makeParseState None [] []
+        parse' st0 s
 
     /// Write a single token. The token bytestring excludes the
     /// surrounding punctuation, so we'll add that back in. This
@@ -278,12 +273,6 @@ module Parser =
             ByteStream.writeByte (byte '"') dst
             ByteStream.writeBytes s dst
             ByteStream.writeByte (byte '"') dst
-        | TT.LblPut ->
-            ByteStream.writeByte (byte ':') dst
-            ByteStream.writeBytes s dst
-        | TT.LblGet ->
-            ByteStream.writeByte (byte '.') dst
-            ByteStream.writeBytes s dst
         | TT.BinRef ->
             ByteStream.writeByte (byte '%') dst
             ByteStream.writeBytes s dst
@@ -298,26 +287,28 @@ module Parser =
 
     let rec private wloop cx p dst =
         match p with
-        | (a :: p') ->
-            match a with
-            | Block b -> 
-                ByteStream.writeByte (byte '[') dst
-                wloop (p' :: cx) b dst
-            | NS (w,a') ->
-                ByteStream.writeBytes w dst
-                ByteStream.writeByte (byte '/') dst
-                wloop cx (a' :: p') dst
-            | Atom t ->
-                writeToken t dst
-                wsp p' dst
-                wloop cx p' dst
-        | [] ->
-            match cx with
-            | (p' :: cx') ->
-                ByteStream.writeByte (byte ']') dst
-                wsp p' dst
-                wloop cx' p' dst
-            | [] -> () // done!
+        | (op :: p') -> wloopOp cx p' op dst
+        | [] -> wloopCx cx dst
+    and private wloopOp cx p op dst =
+        match op with
+        | Block b ->
+            ByteStream.writeByte (byte '[') dst
+            wloop (p :: cx) b dst
+        | NS (struct(w,op')) ->
+            ByteStream.writeBytes w dst
+            ByteStream.writeByte (byte '/') dst
+            wloopOp cx p op' dst
+        | Atom tok ->
+            writeToken tok dst
+            wsp p dst
+            wloop cx p dst
+    and private wloopCx cx dst =
+        match cx with
+        | (p' :: cx') ->
+            ByteStream.writeByte (byte ']') dst
+            wsp p' dst
+            wloop cx' p' dst
+        | [] -> () // done writing
  
     /// Write a program to a byte stream.
     ///
@@ -329,4 +320,90 @@ module Parser =
     /// Write to byte string. (Trivially wraps write'.)
     let inline write (p:Program) : ByteString = 
         ByteStream.write (write' p) 
+
+
+    /// A cursor represents a location within an "open" program. This
+    /// is convenient for rewriting a program, or for addending one,
+    /// while controlling use of the .Net stack.
+    ///
+    /// This cursor is placed between actions, potentially deep in a 
+    /// block. It does not track location within a text, word, or the
+    /// namespace part.
+    [<Struct>]
+    type Cursor =
+        { bcx : (struct(Word list * Cursor)) option
+          toL : Action list // reverse-ordered
+          toR : Action list // program order
+        }
+
+    module Cursor =
+        let inline hasLeft (crs:Cursor) : bool =
+            not (List.isEmpty (crs.toL))
+
+        let inline hasRight (crs:Cursor) : bool =
+            not (List.isEmpty (crs.toR))
+
+        let inline inBlock (crs:Cursor) : bool =
+            Option.isSome (crs.bcx)
+
+        let inline putLeft (op:Action) (crs:Cursor) : Cursor =
+            { crs with toL = (op::crs.toL) }
+
+        let inline putRight (op:Action) (crs:Cursor) : Cursor =
+            { crs with toR = (op::crs.toR) }
+
+        let rec private openBlock' ns op =
+            match op with
+            | Block b -> struct(ns,b)
+            | NS(w,op') -> openBlock' (w::ns) op'
+            | Atom _ -> failwith "not a block"
+
+        let inline private openBlock op = openBlock' (List.empty) op
+
+        let inline enterBlock' (ns:Word list) (b:Program) (crs:Cursor) : Cursor =
+            { bcx = Some(ns,crs)
+              toL = List.empty
+              toR = b
+            }
+
+        let enterBlock (blockOp:Action) (crs:Cursor) : Cursor =
+            let struct(ns,b) = openBlock blockOp
+            enterBlock' ns b crs
+
+        /// same as List.append (List.rev xs) dst
+        let rec appendRev xs dst =
+            match xs with
+            | (x::xs') -> appendRev xs' (x::dst)
+            | _ -> dst
+
+        /// we'll exit to the right of our block
+        let exitBlock (crs:Cursor) : Cursor =
+            match crs.bcx with
+            | Some(ns,crs') ->
+                let b = appendRev (crs.toL) (crs.toR)
+                putLeft (wrapNS ns (Block b)) crs'
+            | None -> failwith "not in a block"
+
+        let skipToBlockStart (crs:Cursor) : Cursor =
+            { crs with toL = List.empty; toR = appendRev (crs.toL) (crs.toR) }
+
+        let skipToBlockEnd (crs:Cursor) : Cursor =
+            { crs with toR = List.empty; toL = appendRev (crs.toR) (crs.toL) }
+
+        let rec private riseToProgRoot (crs:Cursor) : Cursor =
+            if not (inBlock crs) then crs else
+            riseToProgRoot (exitBlock crs)
+
+        let skipToProgStart (crs:Cursor) : Cursor = 
+            riseToProgRoot crs |> skipToBlockStart
+
+        let skipToProgEnd (crs:Cursor) : Cursor = 
+            riseToProgRoot crs |> skipToBlockEnd
+
+        let fromProgram (p:Program) : Cursor =
+            { bcx = None; toL = List.empty; toR = p }
+
+        let toProgram (crs:Cursor) : Program =
+            (skipToProgStart crs).toR
+
 
